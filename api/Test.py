f26d7552436759f0b278ce969c997be8418f281c
zz <pengzhong2010@gmail.com>
2016-08-17 18:02:39 +0800

    update

--- api/Test.py
+++ api/Test.py
@@ -9,11 +9,19 @@ import urllib
 import json
 import datetime
 import time
+import sys
+import os
+sys.path.append(os.path.expanduser('/data/dev/pyspider'))
+from util import ItemIDExtractor
 
 class Index(tornado.web.RequestHandler):
 
     def get(self):
-        body='bbbcccxxx'
+        itemid_extractor = ItemIDExtractor()
+        retcode, itemid = itemid_extractor.extract("datagrnddoc",
+                                                   "http://www.datagrand.com/blog/smg-the-next-unicorn.html")
+
+        body='itemid='+itemid+',retcode='+retcode
         self.write("""
     <div style="text-align: center">
         %s
