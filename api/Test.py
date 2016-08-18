a4efa57ee566fe614e720622c1e9e6b6ffe94a4f
zz <pengzhong2010@gmail.com>
2016-08-18 15:39:12 +0800

    update

--- api/Test.py
+++ api/Test.py
@@ -14,10 +14,10 @@ import os
 sys.path.append(os.path.expanduser('/data/dev/pyspider'))
 from util import ItemIDExtractor
 
-from rec_driver import *
+# from rec_driver import *
 # from pyredis import RedisKv
 
-from pymysql import PyMysql
+# from pymysql import PyMysql
 
 class Index(tornado.web.RequestHandler):
 
