a7325d3b059964f9074b395ef8ba7fc852a9acca
zz <pengzhong2010@gmail.com>
2016-08-18 18:56:38 +0800

    update

--- api/Recommend.py
+++ api/Recommend.py
@@ -1,3 +1,5 @@
+# -*- coding:utf-8 -*-
+
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -9,10 +11,10 @@ import urllib
 import json
 import datetime
 import time
-import sys
-import os
-sys.path.append(os.path.expanduser('/data/dev/pyspider'))
-from util import ItemIDExtractor
+# import sys
+# import os
+# sys.path.append(os.path.expanduser('/data/dev/pyspider'))
+# from util import ItemIDExtractor
 
 class Index(tornado.web.RequestHandler):
 
@@ -25,6 +27,8 @@ class Index(tornado.web.RequestHandler):
         # appname = self.get_argument('appname')
         # appid = self.get_argument('appid')
         # data_url = self.get_argument('url')
+        # cnt = self.get_argument('cnt')
+        cnt=10
 
         # #get itemid
         itemid_extractor = ItemIDExtractor()
@@ -38,7 +42,7 @@ class Index(tornado.web.RequestHandler):
             self.write("url not exists")
             self.finish()
 
-        rec_get_query_url=self.rec_url+'?itemid='+itemid
+        rec_get_query_url=self.rec_url+'?itemid='+itemid+'&cnt='+cnt
 
         client = tornado.httpclient.AsyncHTTPClient()
         response = yield tornado.gen.Task(client.fetch,
@@ -46,5 +50,36 @@ class Index(tornado.web.RequestHandler):
 
     #     # body = json.loads(response.body)
     #     body=response.body
-        self.write(response.body)
+
+        rec_str=response.body
+        rec_dict = json.loads(rec_str)
+        rec_status = rec_dict.get('status')
+        if not rec_status:
+            self.write("rec faild")
+            self.finish()
+        if rec_status == 'FAIL':
+            self.write("rec status FAIL")
+            self.finish()
+        rec_recdata = rec_dict.get('recdata')
+        if not rec_recdata:
+            self.write("rec data not exist")
+            self.finish()
+        if len(rec_recdata)==0:
+            self.write("rec data not exist")
+            self.finish()
+        rec_items_list=[]
+        for i in rec_recdata:
+            rec_items_list.append(i['itemid'])
+
+        self.write(rec_items_list)
         self.finish()
+
+
+
+
+
+
+
+
+
+
