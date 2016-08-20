8bfc86f97d764a845de7e8e38531df8ff2ee21d1
zz <pengzhong2010@gmail.com>
2016-08-20 21:16:23 +0800

    update

--- api/Recommend.py
+++ api/Recommend.py
@@ -15,8 +15,8 @@ import sys
 import os
 import hashlib
 import types
-# sys.path.append(os.path.expanduser('/data/dev/pyspider'))
-# from util import ItemIDExtractor
+sys.path.append(os.path.expanduser('/data/dev/pyspider'))
+from util import ItemIDExtractor
 
 from lib.cache import CACHE
 from db.datacenter import DBCENTERREAD
@@ -36,10 +36,6 @@ class Index(tornado.web.RequestHandler):
         data_url = self.get_argument('url')
         cnt = self.get_argument('cnt')
 
-        # appname='datagranddoc'
-        # appid='1666500'
-        # data_url='http://www.datagrand.com/blog/datagrande-query-2.html'
-        # cnt=10
 
         #url cache
         data_url_md5=self.cache_key_build(str(appname+appid+data_url), 'url')
@@ -50,33 +46,33 @@ class Index(tornado.web.RequestHandler):
             return
 
 
-        # #get itemid
-        # itemid_extractor = ItemIDExtractor()
-        # retcode, itemid = itemid_extractor.extract(appname,
-        #                                            data_url)
-        # # print retcode
-        # # print itemid
-        # # self.finish()
-        # if retcode:
-        # #66
-        #     res = self.res_formate_dict("FAIL", [], '66')
-        #     res_json = json.dumps(res)
-        #     CACHE.instance().set(data_url_md5, res_json, 7200)
-        #     self.write(res_json)
-        #     self.finish()
-        #     return
-        #
-        # rec_get_query_url=self.rec_url+str(appname)+'?itemid='+itemid+'&cnt='+str(cnt)
-        #
-        # client = tornado.httpclient.AsyncHTTPClient()
-        # response = yield tornado.gen.Task(client.fetch,
-        #                                   rec_get_query_url )
+        #get itemid
+        itemid_extractor = ItemIDExtractor()
+        retcode, itemid = itemid_extractor.extract(appname,
+                                                   data_url)
+        # print retcode
+        # print itemid
+        # self.finish()
+        if retcode:
+        #66
+            res = self.res_formate_dict("FAIL", [], '66')
+            res_json = json.dumps(res)
+            CACHE.instance().set(data_url_md5, res_json, 7200)
+            self.write(res_json)
+            self.finish()
+            return
+
+        rec_get_query_url=self.rec_url+str(appname)+'?itemid='+itemid+'&cnt='+str(cnt)
+
+        client = tornado.httpclient.AsyncHTTPClient()
+        response = yield tornado.gen.Task(client.fetch,
+                                          rec_get_query_url )
 
 
 
         # # rec_str = '{"status": "WARN", "errors": {"message": "cnt illegal ,set to 20", "code": -2}, "recdata": [{"itemid": "c41bc01aea03492d27890f09004c7737", "rsn": ""}, {"itemid": "04afd591a784d96080cfc987cd800050", "rsn": ""}, {"itemid": "58585e57794d6fef3f01dc96a894d67d", "rsn": ""}, {"itemid": "146fa524614f230b97e1ec1bb888b440", "rsn": ""}, {"itemid": "97fbfd6fc61da72e4b20df682e408793", "rsn": ""}, {"itemid": "8a312d906721344d0577ae458cf8cf12", "rsn": ""}, {"itemid": "30e92d7e57c180242651cde1eadd3833", "rsn": ""}, {"itemid": "4e1e894be752051a085da475148f9ac0", "rsn": ""}, {"itemid": "139153f44ba17c6e281a324bdd574955", "rsn": ""}, {"itemid": "77343cb006e277e6799bee36d5b5cce4", "rsn": ""}, {"itemid": "2edc204f61fa2912f78dae50dce0508d", "rsn": ""}, {"itemid": "59a9428b043d4c7be43eed2419da289f", "rsn": ""}, {"itemid": "0352c22abb2b5c4dfb3fe4098aed14ec", "rsn": ""}, {"itemid": "c67385784ac3f774b04df5fcb1dd25d1", "rsn": ""}, {"itemid": "a60120c498cf55bacda594122e3345e8", "rsn": ""}, {"itemid": "d6cec483cbfe1c4bfebbeda869d16b7b", "rsn": ""}, {"itemid": "2389cc0e43214306ba25ec1a64e814f8", "rsn": ""}, {"itemid": "7136981f11b3215af37ba91db39f272b", "rsn": ""}, {"itemid": "6d940dd7cff9f379a8c78927444df666", "rsn": ""}, {"itemid": "19cc91d93461208963a909a29b89d7ee", "rsn": ""}], "request_id": "1471512949576753"}'
-        rec_str = '{"status": "WARN", "errors": {"message": "cnt illegal ,set to 20", "code": -2}, "recdata": [{"itemid": "c41bc01aea03492d27890f09004c7737", "rsn": ""}, {"itemid": "04afd591a784d96080cfc987cd800050", "rsn": ""}], "request_id": "1471512949576753"}'
-        # rec_str=response.body
+        # rec_str = '{"status": "WARN", "errors": {"message": "cnt illegal ,set to 20", "code": -2}, "recdata": [{"itemid": "c41bc01aea03492d27890f09004c7737", "rsn": ""}, {"itemid": "04afd591a784d96080cfc987cd800050", "rsn": ""}], "request_id": "1471512949576753"}'
+        rec_str=response.body
         rec_dict = json.loads(rec_str)
         rec_status = rec_dict.get('status')
 
