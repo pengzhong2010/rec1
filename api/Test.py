2e5c0acd057e2f1c3c30cd08b8b156b87baf9519
zz <pengzhong2010@gmail.com>
2016-08-22 17:34:01 +0800

    update

--- api/Test.py
+++ api/Test.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -9,26 +10,41 @@ import urllib
 import json
 import datetime
 import time
-import sys
-import os
-sys.path.append(os.path.expanduser('/data/dev/pyspider'))
-from util import ItemIDExtractor
+# import sys
+# import os
+# sys.path.append(os.path.expanduser('/data/dev/pyspider'))
+# from util import ItemIDExtractor
 
-# from rec_driver import *
+# from lib.log import LOG
+from lib.rec_driver import *
 # from pyredis import RedisKv
 
-# from pymysql import PyMysql
+from lib.pymysql import PyMysql
+
+from lib.common import base_path
 
 class Index(tornado.web.RequestHandler):
-
+    mysql_con=''
     def get(self):
-        itemid_extractor = ItemIDExtractor()
-        retcode, itemid = itemid_extractor.extract("datagrnddoc",
-                                                   "http://www.datagrand.com/blog/smg-the-next-unicorn.html")
-        print retcode
-        print itemid
-        body='ddd'
-        self.write("""
-    <div style="text-align: center">
-        %s
-    </div>""" % (body))
+        # pass
+
+        print base_path()
+        
+        # LOG.instance().send_task('wlog.write_log', args=["abc"])
+        # LOG.ilog("aaaaaa")
+
+        # self.mysql_con = PyMysql("rr-2zeq53lf7562ks1ko.mysql.rds.aliyuncs.com", 3306, "siterec", "siterec123456",
+        #                          "siterec_datacenter")
+        # sql = " select * from item_info limit 1"
+        # # print sql
+        # ret = self.mysql_con.select(sql)
+        # print ret
+
+        return
+
+
+
+
+
+
+
