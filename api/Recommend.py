5b3b4d8297174dd78881243ec26c7b780d2c736b
zz <pengzhong2010@gmail.com>
2016-08-23 10:16:20 +0800

    update

--- api/Recommend.py
+++ api/Recommend.py
@@ -22,6 +22,55 @@ from lib.cache import CACHE
 from db.datacenter import DBCENTERREAD
 from lib.log import LOG
 
+
+def res_formate_dict(status, res_list=None, code=None):
+    res_dict = {}
+    res_dict['status'] = status
+
+    if status == 'FAIL':
+        res_dict['errors'] = {}
+        if code == '66':
+            res_dict['errors']['code'] = code
+            res_dict['errors']['message'] = 'url not exists'
+        elif code == '67':
+            res_dict['errors']['code'] = code
+            res_dict['errors']['message'] = 'rec status faild'
+        elif code == '68':
+            res_dict['errors']['code'] = code
+            res_dict['errors']['message'] = 'rec data not exist'
+        elif code == '69':
+            res_dict['errors']['code'] = code
+            res_dict['errors']['message'] = 'cookie not exists'
+
+        return res_dict
+    elif status == 'OK':
+        res_dict['rec_data'] = res_list
+        return res_dict
+    else:
+        return {}
+
+
+def str_md5(str):
+    if type(str) is types.StringType:
+        m = hashlib.md5()
+        m.update(str)
+        return m.hexdigest()
+    else:
+        return ''
+
+
+def cache_key_build(str, type):
+    item_key = ''
+    if type == 'item':
+        item_key = 'ITEM' + str
+    elif type == 'url':
+        item_key = str_md5(str)
+        if item_key:
+            item_key = 'SURL' + item_key
+
+    return item_key
+
+
 class Index(tornado.web.RequestHandler):
 
     rec_url = 'http://recapi.datagrand.com/relate/'
@@ -40,7 +89,7 @@ class Index(tornado.web.RequestHandler):
         self.set_header_orgin()
 
         #url cache
-        data_url_md5=self.cache_key_build(str(appname+appid+data_url+cnt), 'url')
+        data_url_md5=cache_key_build(str(appname+appid+data_url+cnt), 'url')
         res_json = CACHE.instance().get_local(data_url_md5)
         if res_json:
             request_path = self.request.path
@@ -59,7 +108,7 @@ class Index(tornado.web.RequestHandler):
 
         if retcode:
         #66
-            res = self.res_formate_dict("FAIL", [], '66')
+            res = res_formate_dict("FAIL", [], '66')
             self.res_write(res, data_url_md5)
             return
 
@@ -79,7 +128,7 @@ class Index(tornado.web.RequestHandler):
 
         if (not rec_status) or (rec_status == 'FAIL'):
             #67
-            res = self.res_formate_dict("FAIL", [], '67')
+            res = res_formate_dict("FAIL", [], '67')
             self.res_write(res, data_url_md5)
             return
 
@@ -87,7 +136,7 @@ class Index(tornado.web.RequestHandler):
 
         if (not rec_recdata) or (len(rec_recdata)==0):
             #68
-            res = self.res_formate_dict("FAIL", [], '68')
+            res = res_formate_dict("FAIL", [], '68')
             self.res_write(res, data_url_md5)
             return
 
@@ -101,7 +150,7 @@ class Index(tornado.web.RequestHandler):
         db_select_items_list=[]
         #cache
         for i in rec_items_list:
-            data_item_md5 = self.cache_key_build(str(i), 'item')
+            data_item_md5 = cache_key_build(str(i), 'item')
             item_json = CACHE.instance().get_local(data_item_md5)
             if item_json:
                 rec_items_dict[i] = json.loads(item_json)
@@ -137,7 +186,7 @@ class Index(tornado.web.RequestHandler):
                     if itemid_tmp:
                         rec_items_dict[itemid_tmp]=i
                         # cache
-                        data_item_md5 = self.cache_key_build(str(itemid_tmp), 'item')
+                        data_item_md5 = cache_key_build(str(itemid_tmp), 'item')
                         CACHE.instance().set(data_item_md5, json.dumps(i), 7200)
 
         res_list=[]
@@ -146,54 +195,13 @@ class Index(tornado.web.RequestHandler):
                 res_list.append(rec_items_dict[i])
 
 
-        res=self.res_formate_dict("OK",res_list)
+        res=res_formate_dict("OK",res_list)
         self.res_write(res,data_url_md5)
         return
 
 
 
-    def res_formate_dict(self,status,res_list=None,code=None):
-        res_dict={}
-        res_dict['status']=status
 
-        if status=='FAIL':
-            res_dict['errors']={}
-            if code == '66':
-                res_dict['errors']['code'] = code
-                res_dict['errors']['message'] = 'url not exists'
-            elif code == '67':
-                res_dict['errors']['code']=code
-                res_dict['errors']['message'] = 'rec status faild'
-            elif code == '68':
-                res_dict['errors']['code'] = code
-                res_dict['errors']['message'] = 'rec data not exist'
-
-            return res_dict
-        elif status=='OK':
-            res_dict['rec_data'] = res_list
-            return res_dict
-        else:
-            return {}
-
-    def str_md5(self,str):
-
-        if type(str) is types.StringType:
-            m = hashlib.md5()
-            m.update(str)
-            return m.hexdigest()
-        else:
-            return ''
-
-    def cache_key_build(self,str,type):
-        item_key=''
-        if type=='item':
-            item_key = 'ITEM' + str
-        elif type=='url':
-            item_key=self.str_md5(str)
-            if item_key:
-                item_key='SURL'+item_key
-
-        return item_key
 
     def res_write(self,res,data_url_md5):
         res_json = json.dumps(res)
@@ -227,15 +235,17 @@ class Personalized(tornado.web.RequestHandler):
     def get(self):
         # pass
         appname = self.get_argument('appname')
+        appid = self.get_argument('appid')
         cnt = self.get_argument('cnt')
 
-
-        cookie = self.request.headers.get('Cookie')
+        # header = self.request.headers
+        # print header
+        cookie = self.get_cookie('datagrand_uid')
         if not cookie:
-            res = self.res_formate_dict("FAIL", [], '69')
+            res = res_formate_dict("FAIL", [], '69')
             self.res_write(res)
             return
-
+        # print cookie
         self.set_header_orgin()
 
         cookie=str(cookie)
@@ -245,39 +255,86 @@ class Personalized(tornado.web.RequestHandler):
         response = yield tornado.gen.Task(client.fetch,
                                           rec_get_query_url )
 
-        rec_data=str(response.body)
-        res = self.res_formate_dict("OK", rec_data)
+
+        # rec_str = '{\"status\": \"OK\", \"recdata\": [{\"itemid\": \"05f7f3ebf5fb2bc0de08eaccb7e207bd\", \"rsn\": \"\"}, {\"itemid\": \"04afd591a784d96080cfc987cd800050\", \"rsn\": \"\"}, {\"itemid\": \"7136981f11b3215af37ba91db39f272b\", \"rsn\": \"\"}, {\"itemid\": \"19cc91d93461208963a909a29b89d7ee\", \"rsn\": \"\"}, {\"itemid\": \"19e92124f69676349cd6ff6210cd79b1\", \"rsn\": \"\"}, {\"itemid\": \"c5fb89c658e77d9d84805ee5ded7566d\", \"rsn\": \"\"}, {\"itemid\": \"2389cc0e43214306ba25ec1a64e814f8\", \"rsn\": \"\"}, {\"itemid\": \"4e1e894be752051a085da475148f9ac0\", \"rsn\": \"\"}, {\"itemid\": \"eeeac074b30d3afd3afcd01ee94cc9ff\", \"rsn\": \"\"}, {\"itemid\": \"d047c8e12241fc41ac26ad50bc954638\", \"rsn\": \"\"}], \"request_id\": \"1471916927781132\"}'
+        rec_str=response.body
+        rec_dict = json.loads(rec_str)
+        rec_status = rec_dict.get('status')
+
+        if (not rec_status) or (rec_status == 'FAIL'):
+            # 67
+            res = res_formate_dict("FAIL", [], '67')
+            self.res_write(res)
+            return
+
+        rec_recdata = rec_dict.get('recdata')
+
+        if (not rec_recdata) or (len(rec_recdata) == 0):
+            # 68
+            res = res_formate_dict("FAIL", [], '68')
+            self.res_write(res)
+            return
+
+        rec_items_list = []
+        for i in rec_recdata:
+            rec_items_list.append(i['itemid'])
+
+        # select db
+        rec_items_dict = {}
+        db_select_items_list = []
+        # cache
+        for i in rec_items_list:
+            data_item_md5 = cache_key_build(str(i), 'item')
+            item_json = CACHE.instance().get_local(data_item_md5)
+            if item_json:
+                rec_items_dict[i] = json.loads(item_json)
+            else:
+                rec_items_dict[i] = ''
+                db_select_items_list.append(i)
+
+        if db_select_items_list:
+            db_select_items_str = "','".join(db_select_items_list)
+
+            sql = " select * from item_info where appid=%s and itemid in ('%s') " % (
+            int(appid), db_select_items_str)
+            # print sql
+            ret = DBCENTERREAD.instance().select(sql)
+            if ret:
+                for i in ret:
+                    tmp = i.get("other_info")
+                    if tmp:
+                        tmp2 = json.loads(tmp)
+                        i['other_info'] = tmp2
+                    tmp = i.get("last_update_time")
+                    if tmp:
+                        tmp2 = tmp.strftime("%Y-%m-%d %H:%M:%S")
+                        i['last_update_time'] = tmp2
+                    tmp = i.get("item_modify_time")
+                    if tmp:
+                        tmp2 = tmp.strftime("%Y-%m-%d %H:%M:%S")
+                        i['item_modify_time'] = tmp2
+                    tmp = i.get("create_time")
+                    if tmp:
+                        tmp2 = tmp.strftime("%Y-%m-%d %H:%M:%S")
+                        i['create_time'] = tmp2
+                    itemid_tmp = i.get("itemid")
+                    if itemid_tmp:
+                        rec_items_dict[itemid_tmp] = i
+                        # cache
+                        data_item_md5 = cache_key_build(str(itemid_tmp), 'item')
+                        CACHE.instance().set(data_item_md5, json.dumps(i), 7200)
+
+        res_list = []
+        for i in rec_items_list:
+            if rec_items_dict[i]:
+                res_list.append(rec_items_dict[i])
+
+        res = res_formate_dict("OK", res_list)
         self.res_write(res)
         return
 
-    def res_formate_dict(self, status, res_list=None, code=None):
-        res_dict = {}
-        res_dict['status'] = status
 
-        if status == 'FAIL':
-            res_dict['errors'] = {}
-            if code == '69':
-                res_dict['errors']['code'] = code
-                res_dict['errors']['message'] = 'cookie not exists'
-            # elif code == '67':
-            #     res_dict['errors']['code'] = code
-            #     res_dict['errors']['message'] = 'rec status faild'
-            # elif code == '68':
-            #     res_dict['errors']['code'] = code
-            #     res_dict['errors']['message'] = 'rec data not exist'
 
-            return res_dict
-        elif status == 'OK':
-            rec_data = json.loads(res_list)
-            rec_data_personal = rec_data.get("recdata")
-            if rec_data_personal:
-                res_dict['rec_data'] = rec_data_personal
-            rec_data_requireid = rec_data.get("request_id")
-            if rec_data_requireid:
-                res_dict['request_id'] = rec_data_requireid
-            return res_dict
-        else:
-            return {}
 
     def res_write(self, res):
         res_json = json.dumps(res)
